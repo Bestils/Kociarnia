46e731610797d8a48d84c08fbdbfc083479fc3f0
Bestils <thebestilspl@gmail.com>
2018-05-21 20:02:12 +0200

    palilo sie

---
+++ src/main/java/kobietydokodu/koty/KociarniaApplication.java
@@ -0,0 +1,12 @@
+package kobietydokodu.koty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KociarniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KociarniaApplication.class, args);
+	}
+}
